c5f73dd28c1f5cf8035e4a3854ff03f0fb807c82
ronquecj <ronquechristian@gmail.com>
2025-02-24 02:29:51 +0800

    superadmin

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js';
+import SuperAdmin from '../models/SuperAdmin.js';
 
 // READ
 export const getUser = async (req, res) => {
@@ -8,5 +9,15 @@ export const getUser = async (req, res) => {
     res.status(200).json(user);
   } catch (err) {
     res.status(404).json({ message: err.message });
+  }
+};
+
+export const getSuperAdmin = async (req, res) => {
+  try {
+    const superAdmin = await SuperAdmin.find({});
+
+    res.status(200).json(superAdmin);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getUser } from '../controllers/users.js';
+import { getUser, getSuperAdmin } from '../controllers/users.js';
 import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
 
 // READ
+router.get('/superadmin', getSuperAdmin);
 router.get('/:id', getUser);
 
 // UPDATE
