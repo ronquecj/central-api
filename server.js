ddcc3c5509cfbf0f42739c08a14770cedbaf84bc
ronquechristian <ronquechristian@gmail.com>
2025-05-13 11:30:05 +0800

    asdasd

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.use('/uploads', express.static('uploads'));
+app.use('/api/uploads', express.static('uploads'));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
